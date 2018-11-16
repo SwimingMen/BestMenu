0da44c9dfa03dae242a459509958afce3d8e13c4
Chonghua Yu <>
2018-11-16 15:06:40 +0800

    proj init

---
+++ BestMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BestMenu
+//
+//  Created by Chonghua Yu on 2018/11/16.
+//  Copyright © 2018 SwimingMen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BestMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BestMenu
+//
+//  Created by Chonghua Yu on 2018/11/16.
+//  Copyright © 2018 SwimingMen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
